cc01321fda7a1fcfd72e94727be3b517c1141fa6
CodeSandbox <support@codesandbox.io>
2023-04-13 22:01:53 +0000

    "Initial commit"

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import SignForm from "./Components/SignForm";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <SignForm />
+    </div>
+  );
+}
